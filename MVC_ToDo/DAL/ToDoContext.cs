25ba1484ec9dba3d8de0226a1462744641b3c176
grzegorztomaszewski <aroxd3@gmail.com>
2019-03-02 14:37:59 +0100

    constructor

---
+++ MVC_ToDo/DAL/ToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVC_ToDo.Models;
+
+
+namespace MVC_ToDo.DAL
+{
+    public class ToDoContext :DbContext
+    {
+        public ToDoContext(): base("connectionString")
+        {
+
+        }
+        public DbSet<ToDo> ToDo { get; set; }
+    }
+}
